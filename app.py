d4732a6964eaf6abc296b6acac3a04d8506bfb66
Your Name <marianagaleanoc@gmail.com>
2026-03-17 21:26:10 -0700

    created basic book class with its descriptive labels

--- app.py
+++ app.py
@@ -6,6 +6,17 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///library.db'
 db = SQLAlchemy(app)
 
+class Book(db.Model);
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    author = db.Column(db.String(200), nullable=False)
+    isbn = db.Column(db.String(200), nullable=True)
+    genre = db.Column(db.String(200), nullable=True)
+    edition = db.Column(db.String(200), nullable=True)
+
+    def __repr__(self):
+        return f'<Book {self.title}>'
+
 @app.route('/')
 def index():
     return "Running novel yay"
