557b2eae07ccdb01bae859fb5e035e7fde937c8b
Alexander Arciniega <aarciniega354@csu.fullerton.edu>
2026-03-23 21:59:22 +0000

    Added operations for the parent class

--- models/Items.py
+++ models/Items.py
@@ -34,3 +34,34 @@ class Item(db.Model):
         'polymorphic_on': item_type,
         'polymorphic_identity': 'item'
     }
+
+    def __init__(self, title: str, description: str, qty: int, item_type: str):
+        
+        self._item_id       = str(uuid.uuid4())
+        self._title         = title
+        self._description   = description
+        self._qty           = qty
+        self._available_qty = qty
+        self.added_on       = utcnow()
+        self._item_type     = item_type
+
+    def check_availability(self):
+        return self.available_qty > 0
+    
+    def get_details(self):
+        return {
+            "item_id": self.item_id,
+            "title": self.title,
+            "description": self.description,
+            "qty": self.qty,
+            "available_qty": self.available_qty,
+            "added_on": self.added_on.isoformat(),
+            "item_type": self.item_type
+        }
+    
+    def loan(self, user_id):
+        if not self.check_availability():
+            raise Exception(f"{self.title} is not available for loan. Please make a reservation.")
+        self.available_qty -= 1
+        db.session.commit()
+        # Needs to return a transaction record for the loan to be tracked and returned later
