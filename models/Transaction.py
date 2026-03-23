a988ebd105d34c31914abe03739168651ed16227
Alexander Arciniega <aarciniega354@csu.fullerton.edu>
2026-03-23 22:31:07 +0000

    Added transactions file to track when users loan, renew or return items

---
+++ models/Transaction.py
@@ -0,0 +1,17 @@
+from app import db
+from enum import Enum
+from datetime import datetime, timezone
+import uuid
+
+def utcnow():
+    return datetime.now(timezone.utc)
+
+class TransactionType(Enum):
+    LOAN = "Loan"
+    RENEWED = "Renewed"
+    RETURNED = "Returned"
+
+class TransactionStatus(Enum):
+    ACTIVE = "Active"
+    COMPLETED = "Completed"
+    OVERDUE = "Overdue"
