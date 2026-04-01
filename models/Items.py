67bdc774f9cc67b6515d47b74ac77a926013165e
Alexander Arciniega <aarciniega354@csu.fullerton.edu>
2026-04-01 10:12:01 +0000

    Editing the loan and reserve opeartion

--- models/Items.py
+++ models/Items.py
@@ -13,9 +13,9 @@ class MovieFormat(Enum):
     UHD = "4K UHD"
 
 class Condition(Enum):
+    EXCELLENT = "Excellent"
     GOOD = "Good"
     FAIR = "Fair"
-    POOR = "Poor"
 
 class Item(db.Model):
 
@@ -44,9 +44,19 @@ class Item(db.Model):
         self.added_on      = utcnow()
         self.item_type     = item_type
 
-    def check_availability(self):
+    # Returns boolean indicating if the item is currently available for loan (available_qty > 0)
+    def check_availability(self) -> bool:
         return self.available_qty > 0
     
+    # Returns boolean indicating if the item can be renewed based on its type and any specific rules (e.g. max renewals, holds, etc.)
+    # Base method returns False and can be overridden in subclasses for specific item types with their own renewal rules.
+    def is_renewable(self):
+        return False
+    
+    def get_type(self):
+        return self.item_type
+    
+    # Returns item details as a dictionary, used in routes to send data back as JSON
     def get_details(self):
         return {
             "item_id": self.item_id,
@@ -58,9 +68,33 @@ class Item(db.Model):
             "item_type": self.item_type
         }
     
-    def loan(self, user_id):
+    # Creates a loan Transaction for this item and decreases available quantity by 1
+    def loan(self, user_id: str):
+
+        from models.Transaction import Transaction, TransactionType
+
         if not self.check_availability():
             raise Exception(f"{self.title} is not available for loan. Please make a reservation.")
+        
         self.available_qty -= 1
+        transaction = Transaction(
+            user_id=user_id,
+            item_id=self.item_id,
+            transaction_type=TransactionType.LOAN,
+            item_type=self.item_type
+        )
+        db.session.add(transaction)
         db.session.commit()
-        # Needs to return a transaction record for the loan to be tracked and returned later
+        return transaction
+    
+    def reserve(self, user_id: str):
+
+        from models.Reservation import Reservation
+
+        reservation = Reservation(
+            user_id = user_id,
+            item_id = self.item_id
+        )
+        db.session.add(reservation)
+        db.session.commit()
+        return reservation
